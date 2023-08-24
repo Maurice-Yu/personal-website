90dc1e143f57a5641e2b96b810a4ed6ff2ea5683
ThematicMountain1 <khanacademyuser66@gmail.com>
2023-08-25 01:11:24 +0800

    query note

--- mysite/search/query.py
+++ mysite/search/query.py
@@ -1,6 +1,12 @@
 from azure.cosmos import cosmos_client
 import json
 
+# Note: max_item_count didn't seem to work, see line 38 and 44
+# Next Steps: Implement pagination? Tutorial: https://learn.microsoft.com/en-us/azure/cosmos-db/nosql/query/pagination
+# With example code: https://github.com/Azure/azure-sdk-for-python/blob/main/sdk/cosmos/azure-cosmos/samples/examples.py#L89
+# Make sure to do <pip install azure-cosmos> to have the right dependencies
+
+
 CONFIG = {
     "ENDPOINT": "https://anime-offline-db.documents.azure.com:443/",
     "PRIMARYKEY": "phiGGZqgWjHy2woaTdShWZskLC0CAAIjgNyjCUnfvIthKgDkiHy2DqPq7m62Yt3SCmPm5Req4K9BACDbjYgMig==",
@@ -8,9 +14,9 @@ CONFIG = {
     "CONTAINER": "test1"  # Prolly looks more like a name to you
 }
 
-CONTAINER_LINK = f"dbs/{CONFIG['DATABASE']}/colls/{CONFIG['CONTAINER']}"
-FEEDOPTIONS = {}
-FEEDOPTIONS["enable_cross_partition_query"] = True
+#CONTAINER_LINK = f"dbs/{CONFIG['DATABASE']}/colls/{CONFIG['CONTAINER']}"
+#FEEDOPTIONS = {}
+#FEEDOPTIONS["enable_cross_partition_query"] = True
 # There is also a partitionKey Feed Option, but I was unable to figure out how to us it.
 
 QUERY = {
@@ -32,7 +38,7 @@ container_client = db_client.get_container_client(CONFIG['CONTAINER'])
 results = container_client.query_items(max_item_count=10, query=QUERY["query"], enable_cross_partition_query=True)
 
 # Look at your data
-print(list(results))
+#print(list(results))
 
 # You can also use the list as JSON
-#json.dumps(list(results), indent=4)
+print(json.dumps(list(results)[0:10], indent=4)) 
